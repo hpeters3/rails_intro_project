ad686ef6af5d8b7b256d08f28971692f4682e9d6
hpeters <hpeters3@academic.rrc.ca>
2025-07-13 10:03:26 -0500

    Validations

--- app/models/boss.rb
+++ app/models/boss.rb
@@ -1,3 +1,6 @@
 class Boss < ApplicationRecord
   belongs_to :game
+
+  validates :boss_id, presence: true
+  validates :name, presence: true
 end
--- app/models/monster.rb
+++ app/models/monster.rb
@@ -1,4 +1,7 @@
 class Monster < ApplicationRecord
   has_many :zelda_mobs, dependent: :destory
   has_many :games, through: :zelda_mobs
+
+  validates :monster_id, presence: true
+  validates :name, presence: true
 end
